ef5e3d245e8c50b100c65cef235a207e1922d559
MyJules <mudryk200@gmail.com>
2024-08-04 18:29:57 +0200

    Basic url handler

--- src/niebulo/message_handler.go
+++ src/niebulo/message_handler.go
@@ -1,24 +1,37 @@
 package niebulo
 
-import tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+import (
+	"net/url"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+)
 
 func (niebulo *Niebulo) onMessageReceived(message *tgbotapi.Message) {
+	niebulo.logger.Info("Message received: " + message.Text)
+
+	//Handle commands
 	switch message.Command() {
 	case "help", "h":
 		niebulo.onHelpCommand(message)
-	default:
-		niebulo.sendTextOrPanick(message, "Unknown Command")
+	}
+
+	//Handle url
+	_, err := url.ParseRequestURI(message.Text)
+	if err != nil {
+		niebulo.sendTextOrPanic(message, "Paste send URL to audio you want to download from youtube")
 	}
 }
 
 func (niebulo *Niebulo) onHelpCommand(message *tgbotapi.Message) {
-	niebulo.sendTextOrPanick(message, "Paste URL to audio you want to download from youtube")
+	niebulo.sendTextOrPanic(message, "Paste send URL to audio you want to download from youtube")
 }
 
-func (niebulo *Niebulo) sendTextOrPanick(message *tgbotapi.Message, text string) {
+func (niebulo *Niebulo) sendTextOrPanic(message *tgbotapi.Message, text string) {
 	msg := tgbotapi.NewMessage(message.Chat.ID, text)
 
 	if _, err := niebulo.botApi.Send(msg); err != nil {
+		niebulo.logger.Error("Failed to send message")
 		panic(err)
 	}
+	niebulo.logger.Info("Message sent: " + text)
 }
